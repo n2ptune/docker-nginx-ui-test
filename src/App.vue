22238e1cbefc4d1d8327931e8198ccf69348cc13
n2ptune <n2ptune@outlook.com>
2024-07-11 16:53:50 +0900

    0.0.2

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="v0.0.1" />
+  <HelloWorld msg="v0.0.2" />
 </template>
 
 <style scoped>
